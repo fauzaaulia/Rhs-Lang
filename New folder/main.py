dd1e7c7fb3ae2c19f193326ced1388ae05fed139
Fauza Aulia <fauza@students.unnes.ac.id>
2019-05-10 22:48:27 +0700

    add main.py

--- New folder/main.py
+++ New folder/main.py
@@ -2,14 +2,29 @@ import rhs_lexer
 import rhs_parser
 import rhs_interpreter
 
+from sys import *
+
+#DENGAN MASUKAN BAHASAKU.RHS
 lexer = rhs_lexer.BasicLexer()
 parser = rhs_parser.BasicParser()
 env = {}
 
-text = ""
-with open('bahasaku.rhs', 'r') as file:
-    text = file.read()
-#text = input('basic > ')
+file = open(argv[1])
+text = file.readlines()
+for line in text:
+    tree = parser.parse(lexer.tokenize(line))
+    rhs_interpreter.BasicExecute(tree, env)
 
-tree = parser.parse(lexer.tokenize(text))
-rhs_interpreter.BasicExecute(tree, env)
+#MASUKAN LANGSUNG
+# if __name__ == '__main__':
+#     lexer = rhs_lexer.BasicLexer()
+#     parser = rhs_parser.BasicParser()
+#     env = {}
+#     while True:
+#         try:
+#             text = input('RHS > ')
+#         except EOFError:
+#             break
+#         if text:
+#             tree = parser.parse(lexer.tokenize(text))
+#             rhs_interpreter.BasicExecute(tree, env)
